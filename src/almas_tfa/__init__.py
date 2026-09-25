3522e7806c8a546e880ec13085fb4be2b1b05c2a
2926097 <59182504+2926097@users.noreply.github.com>
2026-09-25 16:00:09 +0200

    feat: exponer M24 modelos nulos

--- src/almas_tfa/__init__.py
+++ src/almas_tfa/__init__.py
@@ -36,6 +36,7 @@ from .evidence_handlers import m15_evidence_extraction, m16_dependency_deduplica
 from .counterevidence_handlers import m20_counterevidence
 from .ablation_handlers import ABLATION_RUNS, m22_ablation
 from .time_sensitivity_handlers import m23_time_sensitivity
+from .null_model_handlers import ALLOWED_NULL_MODELS, m24_null_models, wilson_interval
 from .robustness_handlers import m23_time_sensitivity, m24_null_models, wilson_interval
 from .temporal_handlers import m26_temporal_activation, m27_dated_events
 from .final_handlers import m28_doctrine_hermeneutics, m29_viability_reciprocity, m30_report_gate, m31_report
@@ -82,6 +83,9 @@ __all__ = [
     "ABLATION_RUNS",
     "m22_ablation",
     "m23_time_sensitivity",
+    "ALLOWED_NULL_MODELS",
+    "m24_null_models",
+    "wilson_interval",
     "m23_time_sensitivity",
     "m24_null_models",
     "wilson_interval",
