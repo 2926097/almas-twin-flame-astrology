28e29c0171b36515f78a1ef73db78118effbb57a
2926097 <59182504+2926097@users.noreply.github.com>
2026-09-25 16:21:50 +0200

    refactor: exponer M28 desde doctrine_handlers

--- src/almas_tfa/__init__.py
+++ src/almas_tfa/__init__.py
@@ -39,7 +39,8 @@ from .time_sensitivity_handlers import m23_time_sensitivity
 from .null_model_handlers import ALLOWED_NULL_MODELS, m24_null_models, wilson_interval
 from .robustness_index_handlers import m25_robustness
 from .temporal_handlers import m26_temporal_activation, m27_dated_events
-from .final_handlers import m28_doctrine_hermeneutics, m29_viability_reciprocity, m30_report_gate, m31_report
+from .doctrine_handlers import m28_doctrine_hermeneutics
+from .final_handlers import m29_viability_reciprocity, m30_report_gate, m31_report
 from .orchestrator import (
     CanonicalOverwriteError,
     OrchestrationRun,
