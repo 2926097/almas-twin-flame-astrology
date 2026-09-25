ef6de352b391b60f4777b3f8e033e820a975d639
2926097 <59182504+2926097@users.noreply.github.com>
2026-09-25 13:08:01 +0200

    feat: exponer M10 M11 M12

--- src/almas_tfa/__init__.py
+++ src/almas_tfa/__init__.py
@@ -28,6 +28,7 @@ from .module_contract import (
 from .relational_handlers import m03_synastry, m04_nodes_angles_houses_regencies
 from .symmetry_handlers import antiscion_longitude, contra_antiscion_longitude, m05_declinations, m06_antiscia
 from .relationship_chart_handlers import DavisonBackend, DavisonRequest, circular_midpoint, m07_composite, make_m08_davison
+from .draconic_handlers import m10_individual_draconics, m11_natal_draconic_cross, m12_draconic_draconic
 from .orchestrator import (
     CanonicalOverwriteError,
     OrchestrationRun,
@@ -58,6 +59,9 @@ __all__ = [
     "DavisonRequest",
     "DavisonBackend",
     "make_m08_davison",
+    "m10_individual_draconics",
+    "m11_natal_draconic_cross",
+    "m12_draconic_draconic",
     "MODEL_PILLARS",
     "SUPPORTED_THRESHOLDS",
     "ModelScore",
