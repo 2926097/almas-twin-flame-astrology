1aaa2fc6974f5ad2fc765f34c1c875f8d6e16f6a
2926097 <59182504+2926097@users.noreply.github.com>
2026-09-25 17:17:13 +0200

    refactor: exponer M31 desde report_model_handlers

--- src/almas_tfa/__init__.py
+++ src/almas_tfa/__init__.py
@@ -42,7 +42,7 @@ from .temporal_handlers import m26_temporal_activation, m27_dated_events
 from .doctrine_handlers import m28_doctrine_hermeneutics
 from .reality_handlers import m29_viability_reciprocity
 from .report_gate_handlers import m30_report_gate
-from .final_handlers import m31_report
+from .report_model_handlers import m31_report
 from .orchestrator import (
     CanonicalOverwriteError,
     OrchestrationRun,
