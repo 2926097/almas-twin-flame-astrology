2cada698a10b76fe28bbd5ff90faee6dc4d824db
2926097 <59182504+2926097@users.noreply.github.com>
2026-09-25 16:33:52 +0200

    refactor: registrar M29 desde reality_handlers

--- src/almas_tfa/handlers.py
+++ src/almas_tfa/handlers.py
@@ -30,7 +30,8 @@ from .null_model_handlers import m24_null_models
 from .robustness_index_handlers import m25_robustness
 from .temporal_handlers import m26_temporal_activation, m27_dated_events
 from .doctrine_handlers import m28_doctrine_hermeneutics
-from .final_handlers import m29_viability_reciprocity, m30_report_gate, m31_report
+from .reality_handlers import m29_viability_reciprocity
+from .final_handlers import m30_report_gate, m31_report
 
 
 MODELS = ("AF", "KA", "AG", "LG")
